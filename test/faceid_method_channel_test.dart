315115cc489e8143d96442c0001e4957007cf65e
YarnoVdW <yarno.vandeweyer@student.kdg.be>
2024-04-27 17:08:30 +0200

    changed name for publishing

--- test/faceid_method_channel_test.dart
+++ test/faceid_method_channel_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:biometric_auth/faceid_method_channel.dart';
+import 'package:flutter_biometric_auth/faceid_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
